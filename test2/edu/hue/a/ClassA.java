f4501a660b303fd6b9a04b752713e6e04029a7fd
Max-Qin <56754012+Max-Qin@users.noreply.github.com>
2019-11-29 19:54:40 +0800

    Update ClassA.java

--- test2/edu/hue/a/ClassA.java
+++ test2/edu/hue/a/ClassA.java
@@ -1,9 +1,16 @@
 package test2.edu.hue.a;
 
-public class ClassA {
-    public int value ;
+//public class ClassA {
+//    public int value ;
+//    
+//    public void fuzhi(){
+//        this.value = 1;
+//    }
+//}
+
+public interface ClassA{
     
-    public void fuzhi(){
-        this.value = 1;
-    }
+    public void fuzhi();
 }
+
+// 注释部分是1，2，3 步骤  非注释部分为改变为接口之后
