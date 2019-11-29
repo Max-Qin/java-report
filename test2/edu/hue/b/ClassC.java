a2eda58835298e1307f39afc9799065f8423b9ee
Max-Qin <56754012+Max-Qin@users.noreply.github.com>
2019-11-29 19:56:28 +0800

    Update ClassC.java

--- test2/edu/hue/b/ClassC.java
+++ test2/edu/hue/b/ClassC.java
@@ -2,6 +2,24 @@ package test2.edu.hue.b;
 
 import test2.edu.hue.a.*;
 
-public class ClassC extends ClassA {
-    
+//public class ClassC extends ClassA {
+//    
+//    int value;
+//    
+//    public void fuzhi(){
+//        this.value = 3;
+//    } // fuxie
+//    
+//    public void fuzhi(int value){
+//        super.value = this.value + value;
+//    }
+//}
+
+public class ClassC implements ClassA{
+    int value;
+
+    @Override
+    public void fuzhi() {
+        this.value = 3;
+    }
 }
